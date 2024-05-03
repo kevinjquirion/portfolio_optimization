126ac0bfe666b83c6a9af3a55b20989ae6f1e8a9
kquirion <51336053+kquirion@users.noreply.github.com>
2024-05-03 13:58:49 -0400

    Update main.py
    main - compares results from monte carlo and minimization

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
+from sklearn.preprocessing import StandardScaler
 from datetime import datetime,timedelta
-from functions import adj_close,log_daily_change,sharpe_ratio,minimizer
 import matplotlib.pyplot as plt
+import scipy.optimize as sp_opt
+from pprint import pprint
 import yfinance as yf
+import pandas as pd
 import numpy as np
+import palettable
+import functions
 
-tickers = ['SPY','QQQ','BND','GLD','VTI']
+## choose tickers for your portfolio ##
+tickers = ['AAPL','AMZN','MSFT','SQ']
+num_holdings = len(tickers)
 
-years = 1
+## choose dates from which to get data ##
 end_date = datetime.today()
-start_date = end_date - timedelta(days=365*years)
+start_date = end_date - timedelta(365//2)
 
-print("Start Date: {}, End Date: {}".format(start_date,end_date))
+data = functions.asset_data(tickers,start_date,end_date)
+pprint(data.prices.head())
 
-closes = adj_close(tickers,start_date,end_date)
-log_norms = log_daily_change(closes)
-log_norms = log_norms.dropna()
+print("Initial Weights: {}".format(data.weights))
+print("Initial Expected return: {:.2f}".format(data.exp_returns))
+print("Initial Expected Volatility: {:.2f}".format(data.volatility))
+print("Initial Sharpe Ratio: {:.4f}".format(data.sharpe_ratio) )
 
-cov = log_norms.cov()*252
-initial_guess = [ 1/len(tickers) for _ in tickers ]
-weights = minimizer(initial_guess, log_norms, 0., 1.0)
-print(weights)
+print('\n', '#' * 100, '\n')
 
+min_weight = 0.0
+max_weight = 1.0
+initial_weights = [1/num_holdings for _ in range(num_holdings)]
+optimal_weights = functions.optimize_sharpe_ratio(data,initial_weights,min_weight,max_weight)
+data.set_weights(optimal_weights)
+print("Optimal Weights: {}".format(data.weights))
+print("Optimized Expected return: {:.2f}".format(data.exp_returns))
+print("Optimized Expected Volatility: {:.2f}".format(data.volatility))
+print("Optimized Sharpe Ratio: {:.4f}".format(data.sharpe_ratio) )
+
+print('\n', '#' * 100, '\n')
+
+num_iterations = 10000
+
+
+mc,max_sharpe,min_volatility = functions.sharpe_monte_carlo(num_iterations,data)
+
+print("Results for max Sharpe Ratio: \,")
+print(max_sharpe)
+
+print('\n', '#' * 100, '\n')
+
+print("Results for minimum volatility:")
+print(min_volatility)
+
+plt.tight_layout()
+plt.show()
